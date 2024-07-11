cbda46ac3d9ca27d68cabbdc967e021ccd0219f5
Henrique J. Araujo <liderhenrique@gmail.com>
2024-07-11 17:44:11 -0300

    component: modal de confirmação

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,11 @@ import {
   ArrowRight,
   AtSign,
   Calendar,
+  Mail,
   MapPin,
   Plus,
   Settings2,
+  User,
   UserRoundPlus,
   X,
 } from 'lucide-react'
@@ -56,7 +58,9 @@ export function App() {
   }
 
   function removeEmailFromInvites(emailToRemove: string) {
-    const newEmailList = emailsToInvite.filter(email => email !== emailToRemove)
+    const newEmailList = emailsToInvite.filter(
+      (email) => email !== emailToRemove,
+    )
 
     setEmailsToInvite(newEmailList)
   }
@@ -174,7 +178,10 @@ export function App() {
                     className="py-1.5 px-2.5 rounded-md bg-zinc-800 flex items-center gap-2"
                   >
                     <span className="text-zinc-300">{email}</span>
-                    <button onClick={() => removeEmailFromInvites(email)} type="button">
+                    <button
+                      onClick={() => removeEmailFromInvites(email)}
+                      type="button"
+                    >
                       <X className="size-4 text-zinc-400" />
                     </button>
                   </div>
@@ -210,6 +217,65 @@ export function App() {
           </div>
         </div>
       )}
+
+      <div className="fixed inset-0 bg-black/60 flex items-center justify-center">
+        <div className="w-[640px] rounded-xl py-5 px-6 shadow-shape bg-zinc-900 space-y-5">
+          <div className="space-y-2">
+            <div className="flex items-center justify-between">
+              <h2 className="text-lg font-semibold">
+                Confirmar criação da viagem
+              </h2>
+              <button type="button" onClick={handleClosedModalInvite}>
+                <X className="size-5 text-zinc-400" />
+              </button>
+            </div>
+            <p className="text-sm text-zinc-400">
+              Para concluir a criação da viagem para{' '}
+              <span className="font-semibold text-zinc-100">
+                Florianópolis, Brasil
+              </span>{' '}
+              nas datas{' '}
+              <span className="font-semibold text-zinc-100">
+                de 16 a 27 de Agosto de 2024
+              </span>{' '}
+              preencha seus dados abaixo:
+            </p>
+          </div>
+
+          {/* <div className="w-full h-px bg-zinc-800" /> */}
+
+          <div>
+            <form onSubmit={handleAddEmailToInvite} className="space-y-4">
+              <div className="h-14 px-4 bg-zinc-950 border border-zinc-800 rounded-lg flex items-center gap-2">
+                <User className="size-5 text-zinc-400" />
+
+                <input
+                  type="email"
+                  name="email"
+                  placeholder="Seu nome completo"
+                  className="bg-transparent placeholder-zinc-400 outline-none"
+                />
+              </div>
+
+              <div className="h-14 px-4 bg-zinc-950 border border-zinc-800 rounded-lg flex items-center gap-2">
+                <Mail className="size-5 text-zinc-400" />
+                <input
+                  type="email"
+                  name="email"
+                  placeholder="Seu nome completo"
+                  className="bg-transparent placeholder-zinc-400 outline-none"
+                />
+              </div>
+              <button
+                type="submit"
+                className="w-full flex items-center gap-2 justify-center hover:bg-lime-400 bg-lime-300 text-lime-950 rounded-lg px-5 h-11"
+              >
+                Confirmar criação da viagem
+              </button>
+            </form>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
