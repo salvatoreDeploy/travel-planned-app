d6b0f0dd905a55ef96638d5a15b5664e8c61f5b7
Henrique J. Araujo <liderhenrique@gmail.com>
2024-07-19 17:46:30 -0300

    estilização do datepicker

--- src/components/InputSteps/inputDestinationAndDateStep.tsx
+++ src/components/InputSteps/inputDestinationAndDateStep.tsx
@@ -1,5 +1,11 @@
-import { MapPin, Calendar, Settings2, ArrowRight } from 'lucide-react'
+import { MapPin, Calendar, Settings2, ArrowRight, X } from 'lucide-react'
 import { Button } from '../Button'
+import { useState } from 'react'
+import { Modal } from '../Modal/Modal'
+import { ModalHeader } from '../Modal/ModalHeader'
+import { ModalContent } from '../Modal/ModalContent'
+import { DateRange, DayPicker } from 'react-day-picker'
+import 'react-day-picker/dist/style.css'
 
 interface InputDestinationAndDateStepProps {
   isGuestsInputOpen: boolean
@@ -12,6 +18,19 @@ export function InputDestinationAndDateStep({
   handleOpenGuestsInput,
   isGuestsInputOpen,
 }: InputDestinationAndDateStepProps) {
+  const [isDatePickerOpen, setIsDatePickerOpen] = useState(false)
+  const [eventStartAndDates, setEventStartAndDates] = useState<
+    DateRange | undefined
+  >()
+
+  function handleOpenDatePicker() {
+    return setIsDatePickerOpen(true)
+  }
+
+  function handleCloseDatePicker() {
+    return setIsDatePickerOpen(false)
+  }
+
   return (
     <div className="h-16 bg-zinc-900 px-4 rounded-xl flex items-center shadow-shape gap-3">
       <div className="flex items-center gap-2 flex-1">
@@ -24,15 +43,35 @@ export function InputDestinationAndDateStep({
         />
       </div>
 
-      <div className="flex items-center gap-2">
+      <button
+        onClick={handleOpenDatePicker}
+        disabled={isGuestsInputOpen}
+        className="flex items-center gap-2 text-left"
+      >
         <Calendar className="size-5 text-zinc-400" />
-        <input
-          disabled={isGuestsInputOpen}
-          type="text"
-          placeholder="Quando? "
-          className="w-40 bg-transparent placeholder-zinc-400 outline-none"
-        />
-      </div>
+        <span className="text-lg text-zinc-400 w-40">Quando?</span>
+      </button>
+
+      {isDatePickerOpen && (
+        <Modal size="xs">
+          <ModalHeader>
+            <div className="flex items-center justify-between">
+              <h2 className="text-lg font-semibold">Selecione uma data</h2>
+              <button type="button" onClick={handleCloseDatePicker}>
+                <X className="size-5 text-zinc-400" />
+              </button>
+            </div>
+          </ModalHeader>
+
+          <ModalContent>
+            <DayPicker
+              mode="range"
+              selected={eventStartAndDates}
+              onSelect={setEventStartAndDates}
+            />
+          </ModalContent>
+        </Modal>
+      )}
 
       <div className="w-px h-6 bg-zinc-800" />
 
--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -1,15 +1,24 @@
 import { ReactNode } from 'react'
+import { tv, VariantProps } from 'tailwind-variants'
 
-interface ModalProps {
+const modalVariants = tv({
+  base: 'w-[640px] rounded-xl py-5 px-6 shadow-shape bg-zinc-900 space-y-5',
+  variants: {
+    size: {
+      default: 'w-[640px]',
+      xs: 'w-[360px]',
+    },
+  },
+})
+
+interface ModalProps extends VariantProps<typeof modalVariants> {
   children: ReactNode
 }
 
-export function Modal({ children }: ModalProps) {
+export function Modal({ children, size }: ModalProps) {
   return (
     <div className="fixed inset-0 bg-black/60 flex items-center justify-center">
-      <div className="w-[640px] rounded-xl py-5 px-6 shadow-shape bg-zinc-900 space-y-5">
-        {children}
-      </div>
+      <div className={modalVariants({ size })}>{children}</div>
     </div>
   )
 }
